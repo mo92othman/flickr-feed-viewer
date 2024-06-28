320edda9cbbd79ba1ad263f41b0647f4b9daa20d
mo92othman <mo92othman@gmail.com>
2024-06-28 06:01:11 +0200

    Added revalidation to the fetch to get new feeds

--- client/app/api/[tags]/route.ts
+++ client/app/api/[tags]/route.ts
@@ -12,7 +12,7 @@ export const GET = async (
     const tags = params.tags;
     const response = await fetch(
       `https://www.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1&tags=${tags}`,
-      { next: { revalidate: 10 } },
+      { next: { revalidate: 15 } },
     );
     if (!response.ok) {
       throw new Error('Failed to fetch data from Flickr');
--- client/app/api/route.ts
+++ client/app/api/route.ts
@@ -6,7 +6,7 @@ type Error = {
 export const GET = async () => {
   try {
     const apiUrl = `https://www.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1&api_key=${process.env.NEXT_PUBLIC_FLICKR_API_KEY}`;
-    const response = await fetch(apiUrl);
+    const response = await fetch(apiUrl, { next: { revalidate: 15 } },);
     const data = await response.json();
     // Return the fetched data as the response
     return NextResponse.json(data);
